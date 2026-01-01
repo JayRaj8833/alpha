a90ae929289d4426fbc795d5e7176ec58b2e1ae5
JayRaj ❤️ <87997138+JayRaj8833@users.noreply.github.com>
2026-01-01 11:26:13 +0530

    Update alpha.js

--- alpha.js
+++ alpha.js
@@ -13,6 +13,9 @@
   const urlParams = new URLSearchParams(window.location.search);
   const showParam = urlParams.get('per');
   
+  // Debug logging
+  console.log('URL Parameter per:', showParam);
+  
   // Check for existing cookie
   function getCookie(name) {
     const value = `; ${document.cookie}`;
@@ -22,9 +25,11 @@
   }
   
   const cacheCookie = getCookie('page_cache_access');
+  console.log('Cache Cookie:', cacheCookie);
   
   // If no parameter and no valid cookie, redirect or hide content
   if (!showParam && !cacheCookie) {
+    console.log('No access - hiding content');
     // Hide all content
     document.body.style.display = 'none';
     
@@ -34,6 +39,9 @@
         <div style="text-align: center;">
           <h1>Access Restricted</h1>
           <p>This page requires special access permissions.</p>
+          <p style="color: #ec567c; font-size: 14px; margin-top: 20px;">
+            Please use the correct access URL
+          </p>
         </div>
       </div>
     `;
@@ -44,43 +52,47 @@
   
   // If parameter is present, set cookie for 1 minute
   if (showParam === 'id') {
+    console.log('Parameter found, setting cookie');
     const now = new Date();
     const expiry = new Date(now.getTime() + 1 * 60 * 1000); // 1 minute
     
-    document.cookie = `page_cache_access=true; expires=${expiry.toUTCString()}; path=/; SameSite=Strict`;
+    // Set cookie with domain and path for wider accessibility
+    document.cookie = `page_cache_access=true; expires=${expiry.toUTCString()}; path=/; domain=${window.location.hostname}; SameSite=Lax`;
+    
+    console.log('Cookie set with expiry:', expiry.toUTCString());
     
     // Optional: Remove parameter from URL without reloading
     if (window.history.replaceState) {
-      const newUrl = window.location.pathname + window.location.hash;
+      const newUrl = window.location.pathname;
+      console.log('Cleaning URL from', window.location.href, 'to', newUrl);
       window.history.replaceState({}, document.title, newUrl);
     }
   }
   
-  // If cookie exists but is expired, clear it
+  // If cookie exists, check if it's still valid
   if (cacheCookie) {
-    const cookieExpiry = getCookieExpiry('page_cache_access');
-    if (cookieExpiry && new Date() > cookieExpiry) {
-      document.cookie = 'page_cache_access=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-      location.reload();
-    }
-  }
-  
-  // Helper function to get cookie expiry
-  function getCookieExpiry(name) {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.startsWith(name + '=')) {
-        const cookieParts = cookie.split(';');
-        for (let j = 0; j < cookieParts.length; j++) {
-          const part = cookieParts[j].trim();
-          if (part.startsWith('expires=')) {
-            return new Date(part.substring(8));
-          }
-        }
+    console.log('Cookie exists, checking validity');
+    
+    // Simple check - if cookie exists, grant access for this session
+    // The cookie will expire automatically based on browser's expiry time
+    
+    // For extra safety, you can check expiry time from cookie string
+    const cookieString = document.cookie;
+    const cookieMatch = cookieString.match(/page_cache_access=true(?:; expires=([^;]+))?/);
+    
+    if (cookieMatch && cookieMatch[1]) {
+      const expiryTime = new Date(cookieMatch[1]);
+      const now = new Date();
+      
+      console.log('Cookie expiry:', expiryTime);
+      console.log('Current time:', now);
+      
+      if (now > expiryTime) {
+        console.log('Cookie expired, clearing it');
+        document.cookie = 'page_cache_access=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=' + window.location.hostname + ';';
+        location.reload();
       }
     }
-    return null;
   }
 })();
 
@@ -133,10 +145,26 @@ window.onload = function() {
 // Random profile display code
 document.addEventListener('DOMContentLoaded', function() {
     const profiles = [
-        // ... your profiles array remains unchanged ...
         { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/1.webp", randomname:"Takara Kairi", randomviews:"957K Views", randomrating:"91%", randomquality:"1080p", randomtime:"11 Min", randomvidku:"21 Videos", randomtitle:"Sam Shock Thoroughly Spoils Slim Beauty Japanese Takara Kairi", randomcomment:"123 Comment", randomlike:"100K", randomsecond:"10:36", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/1.png"},
         { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/2.webp", randomname:"Fumiko Sora", randomviews:"475K Views", randomrating:"92%", randomquality:"1080p", randomtime:"12 Min", randomvidku:"21 Videos", randomtitle:"Sexy Japanese Fumiko Sora is learning some card tricks and getting fucked hard", randomcomment:"123 Comment", randomlike:"151K", randomsecond:"11:59", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/2.png"},
-        // ... rest of profiles ...
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/3.webp", randomname:"Harumi Aina", randomviews:"198K Views", randomrating:"93%", randomquality:"1080p", randomtime:"13 Min", randomvidku:"23 Videos", randomtitle:"Asian celebrity Harumi Aina having passionate sex in hot scene with her partner", randomcomment:"123 Comment", randomlike:"123K", randomsecond:"12:54", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/3.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/4.webp", randomname:"Akari Nara", randomviews:"946K Views", randomrating:"94%", randomquality:"1080p", randomtime:"14 Min", randomvidku:"24 Videos", randomtitle:"Akari Nara is an Asian nympho who craves anal creampie", randomcomment:"123 Comment", randomlike:"155K", randomsecond:"13:12", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/4.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/5.webp", randomname:"Hana Makaira", randomviews:"912K Views", randomrating:"95%", randomquality:"1080p", randomtime:"15 Min", randomvidku:"25 Videos", randomtitle:"Sexy Japanese Teen Hana Makaira learning some card tricks and getting fucked hard", randomcomment:"123 Comment", randomlike:"165K", randomsecond:"14:21", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/5.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/6.webp", randomname:"Keiko Kana", randomviews:"875K Views", randomrating:"96%", randomquality:"1080p", randomtime:"16 Min", randomvidku:"26 Videos", randomtitle:"Raven haired Vicki Chase is sucking and riding Dan Damage's cock", randomcomment:"146K Comment", randomlike:"171K", randomsecond:"15:18", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/6.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/7.webp", randomname:"Reina Akiara", randomviews:"890K Views", randomrating:"97%", randomquality:"1080p", randomtime:"17 Min", randomvidku:"27 Videos", randomtitle:"Japan beauty Reina Akiar is riding big black cock Isiah Maxwell", randomcomment:"123 Comment", randomlike:"181K", randomsecond:"16:29", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/7.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/8.webp", randomname:"Sada Aika", randomviews:"936K Views", randomrating:"98%", randomquality:"1080p", randomtime:"18 Min", randomvidku:"28 Videos", randomtitle:"JAPANESE teen Sada Aika is having much fun with a bbc guy while no one's at home", randomcomment:"123 Comment", randomlike:"191K", randomsecond:"17:51", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/8.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/9.webp", randomname:"Seina Amaya", randomviews:"789K Views", randomrating:"99%", randomquality:"1080p", randomtime:"19 Min", randomvidku:"29 Videos", randomtitle:"Sexy Japanese Seina Amaya is learning some card tricks and getting fucked hard", randomcomment:"123 Comment", randomlike:"145K", randomsecond:"18:48", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/9.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/10.webp", randomname:"Gina Ayumi", randomviews:"998K Views", randomrating:"91%", randomquality:"1080p", randomtime:"20 Min", randomvidku:"21 Videos", randomtitle:"Sexy Gina Ayumi opens her mouth to swallow massive load", randomcomment:"123 Comment", randomlike:"197K", randomsecond:"19:23", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/10.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/11.webp", randomname:"Takara Kairi", randomviews:"957K Views", randomrating:"91%", randomquality:"1080p", randomtime:"11 Min", randomvidku:"21 Videos", randomtitle:"Celebrity office lady has unexpectedly naughty sex!", randomcomment:"123 Comment", randomlike:"100K", randomsecond:"10:36", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/11.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/12.webp", randomname:"Fumiko Sora", randomviews:"475K Views", randomrating:"92%", randomquality:"1080p", randomtime:"12 Min", randomvidku:"22 Videos", randomtitle:"Please Don't Cum Inside Me?? Japanese School Girl Gets Pussy Fucked Hard With Squirting Orgasms!!!", randomcomment:"123 Comment", randomlike:"151K", randomsecond:"11:59", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/12.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/13.webp", randomname:"Harumi Aina", randomviews:"198K Views", randomrating:"93%", randomquality:"1080p", randomtime:"13 Min", randomvidku:"23 Videos", randomtitle:"Miku Ohashi gets her very first time creampie and Asian end in the best Asian porn!", randomcomment:"123 Comment", randomlike:"123K", randomsecond:"12:54", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/13.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/14.webp", randomname:"Akari Nara", randomviews:"946K Views", randomrating:"94%", randomquality:"1080p", randomtime:"14 Min", randomvidku:"24 Videos", randomtitle:"Horny Asian cosplayer Yui Nishikawa blows minds in extraordinary JAV creampie fuckfest", randomcomment:"123 Comment", randomlike:"155K", randomsecond:"13:12", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/14.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/15.webp", randomname:"Hana Makaira", randomviews:"912K Views", randomrating:"95%", randomquality:"1080p", randomtime:"15 Min", randomvidku:"25 Videos", randomtitle:"Oh My Good! Youre is Beautiful", randomcomment:"123 Comment", randomlike:"165K", randomsecond:"14:21", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/15.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/16.webp", randomname:"Keiko Kana", randomviews:"875K Views", randomrating:"96%", randomquality:"1080p", randomtime:"16 Min", randomvidku:"26 Videos", randomtitle:"I Was Driven Home From College By My Friend", randomcomment:"146K Comment", randomlike:"171K", randomsecond:"15:18", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/16.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/17.webp", randomname:"Reina Akiara", randomviews:"890K Views", randomrating:"97%", randomquality:"1080p", randomtime:"17 Min", randomvidku:"27 Videos", randomtitle:"Lovely bedtime for tonight", randomcomment:"123 Comment", randomlike:"181K", randomsecond:"16:29", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/17.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/18.webp", randomname:"Sada Aika", randomviews:"936K Views", randomrating:"98%", randomquality:"1080p", randomtime:"18 Min", randomvidku:"28 Videos", randomtitle:"Have You Ever Worn A Strap Like Me Now?", randomcomment:"123 Comment", randomlike:"191K", randomsecond:"17:51", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/18.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/19.webp", randomname:"Seina Amaya", randomviews:"789K Views", randomrating:"99%", randomquality:"1080p", randomtime:"19 Min", randomvidku:"29 Videos", randomtitle:"She Was Puzzled By The Situation", randomcomment:"123 Comment", randomlike:"145K", randomsecond:"18:48", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/19.png"},
+        { randomimage:"https://rh.positivetraits.us/wp-content/uploads/2025/16/20.webp", randomname:"Gina Ayumi", randomviews:"998K Views", randomrating:"91%", randomquality:"1080p", randomtime:"20 Min", randomvidku:"21 Videos", randomtitle:"Romantic sex with my cute girlfriend! It feels good and keeps climaxing  ", randomcomment:"123 Comment", randomlike:"197K", randomsecond:"19:23", randomlogo:"https://rh.positivetraits.us/wp-content/plugins/pluginadsen/yphub/img/logos/20.png"}
     ];
     
     function shuffleArray(array) {
